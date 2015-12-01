05a85e863c552e7a67c8cd5b5ad4532cdc7f75fd
Ryan Warren <rrrdees@gmail.com>
2015-12-01 22:17:42 +0000

    commented the gui.py script and fixed the not updating issue

--- docs/gui.py
+++ docs/gui.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+
+# importing the modules and classes
 import pygame
 from pygame.locals import *
 from guibase import Gui
@@ -7,20 +10,31 @@ from guiobject import GuiObject
 ArmEnv = Gui(500,500,(250,250,250), "Telescopic Arm Gui", 30)
 
 # create the arm object
-ArmObj = GuiObject(20, 20, (0,250,0), 0, 0)
+ArmObj = GuiObject(20, 20, (0,0,0), 0, 0)
 
-# initialize the pygame clock
-clock = pygame.time.Clock()
+# update and render everything for the first time - this allows the settings to initialize before the first frame is run
 ArmEnv.update()
+ArmEnv.render([ArmObj])
 
-
+# creating the main while loop
 while True :
+	# getting all the pygame events
 	for event in pygame.event.get() :
+		# checking if the event is to quit pygame - then quit the program
 		if event.type == pygame.QUIT :
 			ArmEnv.terminate()
 	
-	# render everything
+	# all other code shinanigans goes here
+		#...
+			
+	# update everything that needs updating
+	ArmEnv.update()
+	
+	# render everything to the screen
 	ArmEnv.render([ArmObj])
 	
 	# tick the fps
 	ArmEnv.clock.tick(ArmEnv.fps)
+	
+# fail safe quit
+ArmEnv.terminate()
