b84fa50298345e590c062098bd737bc8bd297da8
Winstanley EES <rrrdees@gmail.com>
2015-12-02 12:44:27 +0000

    commenting the gui object class file
    added comments to the gui object class file

--- docs/guiobject.py
+++ docs/guiobject.py
@@ -1,14 +1,18 @@
+#!/usr/bin/python
 import pygame
 
+# the main gui object class
 class GuiObject() :
 
+	# the init function sets the height, width, colour, x and y of the gui object
 	def __init__(self, width, height, colour, x, y) :
 		self.width = width
 		self.height = height
 		self.colour = colour
 		self.x = x
 		self.y = y
-		
+	
+	# the move function will allpw the gui object to move by a specified x and y value
 	def move(self, x, y) :
 		self.x += x
 		self.y += y
