2c4b2a9054c1bf92fd450c0f8fcc25520023943d
Ryan Warren <rrrdees@gmail.com>
2015-12-02 23:05:45 +0000

    added comments to the guiobject class and added border functionality (not actually implemented yet)

--- docs/guiobject.py
+++ docs/guiobject.py
@@ -5,12 +5,13 @@ import pygame
 class GuiObject() :
 
 	# the init function sets the height, width, colour, x and y of the gui object
-	def __init__(self, width, height, colour, x, y) :
+	def __init__(self, width, height, colour, x, y, border) :
 		self.width = width
 		self.height = height
 		self.colour = colour
 		self.x = x
 		self.y = y
+		self.border = border
 	
 	# the move function will allpw the gui object to move by a specified x and y value
 	def move(self, x, y) :
